17e90a7aeba82b2fb3d59cb7d4f3283769f61e43
Martin Rostan <mrostan@infragistics.com>
2021-05-07 16:14:42 -0300

    changed to use mybi app in production too

--- mybi/src/Constants.js
+++ mybi/src/Constants.js
@@ -1,5 +1,5 @@
 const dev_base_url = 'http://localhost:8080/mybi/';
-const prod_base_url = 'http://revealcdb01.eastus.cloudapp.azure.com:8081/mybi-backend/'
+const prod_base_url = 'http://revealcdb01.eastus.cloudapp.azure.com:8081/mybi/'
 const base_url = process.env.NODE_ENV === 'development' ? dev_base_url : prod_base_url;
 const base_reveal_url = base_url + "reveal-api/";
 const base_api_url = base_url + "api/";
