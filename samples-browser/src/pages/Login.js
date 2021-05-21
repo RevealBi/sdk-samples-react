a6406558d61d26dd2cd0f8891ffd084837392046
Martin Rostan <mrostan@infragistics.com>
2021-05-21 16:11:03 -0300

    removed login form, o365 is the only way to login now

--- samples-browser/src/pages/Login.js
+++ samples-browser/src/pages/Login.js
@@ -10,8 +10,6 @@ const Login = () => {
     const [isLoggedIn, setLoggedIn] = useState(false);
     const [isError, setIsError] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");    
     const msalInstance = useMsal().instance;
     const authentication = useAuthentication();
 
@@ -27,17 +25,6 @@ const Login = () => {
         setErrorMessage(errorMessage);
     }
 
-    const usernamePasswordLogin = () => {
-        backend.login(
-            username, 
-            password, 
-            () => {
-                loginCompleted(username, username);
-            }, (e) => {
-                loginFailed(e.toString());
-            }
-        );
-    }
     const msalLogin = () => {
         msalInstance.acquireTokenPopup(msalLoginRequest).then(authResult => {
             var accessToken = authResult.accessToken;
@@ -56,13 +43,6 @@ const Login = () => {
             console.log(e);
         });
     }
-    const passwordOnKeyUp = (e) => {
-        if (e.code === "Enter") {
-            if (username !== null && username !== "" && password !== null && password !== "") {
-                usernamePasswordLogin();
-            }
-        }
-    }
     if (isLoggedIn) {
         return <Redirect to="/"/>;
     } 
@@ -71,13 +51,6 @@ const Login = () => {
             <div className="Login-Card">
                 <div className="Login-Form">
                     <button id="Login_Submit" className="Login-Button-O365" onClick={msalLogin}>Sign In with O365</button>
-                    <input id="Login_Username_Input" className="Login-Input" type="username" value={username} onChange={e => {
-                        setUsername(e.target.value);
-                    }} placeholder="user name" />
-                    <input id="Login_Password_Input" className="Login-Input" type="password" value={password} onChange={e => {
-                        setPassword(e.target.value);
-                    }} onKeyUp={passwordOnKeyUp} placeholder="password" />
-                    <button id="Login_Submit" className="Login-Button" onClick={usernamePasswordLogin}>Sign In</button>
                 </div>
                 { isError &&<div id="Login_Error" className="Login-Error">{errorMessage}</div> }
             </div>
