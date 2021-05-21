955ddaab303012de02d7b40d7f9b1da4db265026
Martin Rostan <mrostan@infragistics.com>
2021-05-21 16:06:12 -0300

    removed file no longer used

--- samples-browser/src/context/dashboardsListState.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const DashboardsListStateContext = createContext();
-
-export const useDashboardsListState = () => {
-    return useContext(DashboardsListStateContext);
-}
