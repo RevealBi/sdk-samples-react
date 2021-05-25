dac95486ccb92dc50763223ff0d0863779dea2af
Martin Rostan <mrostan@infragistics.com>
2021-05-25 14:13:21 -0300

    moved dashboards endpoint under reveal-api, spring is restricted to a single JAX-RS app

--- upmedia-browser/src/Constants.js
+++ upmedia-browser/src/Constants.js
@@ -3,12 +3,11 @@ const prod_base_url = 'https://revealcdb01.eastus.cloudapp.azure.com/upmedia-bac
 const base_url = process.env.NODE_ENV === 'development' ? dev_base_url : prod_base_url;
 
 const router_base_name = process.env.NODE_ENV === 'development' ? '/' : '/upmedia-browser';
-const base_reveal_url = base_url + "reveal-api/";
-const base_api_url = base_url + "api/";
+const base_api_url = base_url + "reveal-api/";
 
 export const config = {
     url: {
-        REVEAL_API_URL: base_reveal_url,
+        REVEAL_API_URL: base_api_url,
         API_URL_DASHBOARDS: base_api_url + 'dashboards',
     },
     routerBasename: router_base_name
