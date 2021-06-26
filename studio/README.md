7865f354214489f29511dc7ebee79e22fb021fda
Martin Rostan <mrostan@infragistics.com>
2021-06-26 15:54:52 -0300

    Studio sample, shows how to create dashboards and data sources

---
+++ studio/README.md
@@ -0,0 +1,3 @@
+# Reveal BI - [RevealBI](https://revealbi.io/) Studio React Application
+This is a sample React application that shows how to use the Reveal BI SDK for Web, including how to create/save dashboards and create data sources.
+
---
+++ studio/src/components/AppHeader.js
@@ -0,0 +1,19 @@
+import '../App.css';
+import { Link } from 'react-router-dom';
+import homeicon from '../images/home-icon.svg';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+function AppHeader(props) {
+    return (
+        <header className="App-Header">
+            { !props.isHome &&
+                <OverlayTrigger placement="bottom" overlay={<Tooltip>Home</Tooltip>}>
+                    <Link id="home_link" to="/" className="App-Header-Home-Link"><img alt="Home" src={homeicon}/></Link>
+                </OverlayTrigger>
+            }
+            <span className="App-Header-Title">{props.title}</span>
+        </header>
+    );
+}
+
+export default AppHeader;
